eb99fcc0a25d9a5e62d638667273abe86f15f9e0
FinE54 <finegn@outlook.com>
2020-04-22 16:19:53 +0800

    Create Plugin files and write

---
+++ src/main/java/moe/fine54/dev/antibreakblock/AntiBreakBlock.java
@@ -0,0 +1,8 @@
+package moe.fine54.dev.antibreakblock;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiBreakBlock extends JavaPlugin implements CommandExecutor {
+
+}
